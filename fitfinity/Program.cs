f50e7685843c3570a1a40e0edd846787164e20f6
TAUHID TAAHA <tauhidtaaha7@gmail.com>
2024-01-15 01:00:37 +0600

    theme applied at the beginning

--- fitfinity/Program.cs
+++ fitfinity/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace fitfinity
 {
@@ -12,13 +14,66 @@ namespace fitfinity
         static void Main(string[] args)
 
         {
+
            
 
+            UserManager userManager = new UserManager();
 
-                    UserManager userManager = new UserManager();
-            
-            Console.WriteLine("Welcome to Calorie Intake Calculator!");
-            Console.WriteLine("=====================================");
+            string name = "███████╗██╗████████╗███████╗██╗███╗   ██╗██╗████████╗██╗   ██╗\r\n██╔════╝██║╚══██╔══╝██╔════╝██║████╗  ██║██║╚══██╔══╝╚██╗ ██╔╝\r\n█████╗  ██║   ██║   █████╗  ██║██╔██╗ ██║██║   ██║    ╚████╔╝ \r\n██╔══╝  ██║   ██║   ██╔══╝  ██║██║╚██╗██║██║   ██║     ╚██╔╝  \r\n██║     ██║   ██║   ██║     ██║██║ ╚████║██║   ██║      ██║   \r\n╚═╝     ╚═╝   ╚═╝   ╚═╝     ╚═╝╚═╝  ╚═══╝╚═╝   ╚═╝      ╚═╝   \r\n                                                              ";
+           // string TITILE = "  _____ ___ _____ _____ ___ _   _ ___ _______   __\r\n|  ___|_ _|_   _|  ___|_ _| \\ | |_ _|_   _\\ \\ / /\r\n| |_   | |  | | | |_   | ||  \\| || |  | |  \\ V / \r\n|  _|  | |  | | |  _|  | || |\\  || |  | |   | |  \r\n|_|   |___| |_| |_|   |___|_| \\_|___| |_|   |_|  \r\n                                                 ";
+            //string motto = "__  __                       ______ _  __                              ______                          __ \r\n\\ \\/ /____   __  __ _____   / ____/(_)/ /_ ____   ___   _____ _____   / ____/_  __ ____   ___   _____ / /_\r\n \\  // __ \\ / / / // ___/  / /_   / // __// __ \\ / _ \\ / ___// ___/  / __/  | |/_// __ \\ / _ \\ / ___// __/\r\n / // /_/ // /_/ // /     / __/  / // /_ / / / //  __/(__  )(__  )  / /___ _>  < / /_/ //  __// /   / /_  \r\n/_/ \\____/ \\__,_//_/     /_/    /_/ \\__//_/ /_/ \\___//____//____/  /_____//_/|_|/ .___/ \\___//_/    \\__/  \r\n                                                                               /_/                        ";
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            AnimateText(name);
+
+            Thread.Sleep(2000);
+            string motto = "┬ ┬┌─┐┬ ┬┬─┐  ┌─┐┬┌┬┐┌┐┌┌─┐┌─┐┌─┐  ┌┬┐┬─┐┌─┐┌─┐┬┌─┌─┐┬─┐\r\n└┬┘│ ││ │├┬┘  ├┤ │ │ │││├┤ └─┐└─┐   │ ├┬┘├─┤│  ├┴┐├┤ ├┬┘\r\n ┴ └─┘└─┘┴└─  └  ┴ ┴ ┘└┘└─┘└─┘└─┘   ┴ ┴└─┴ ┴└─┘┴ ┴└─┘┴└─";
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+          
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkRed ;
+
+            Console.WriteLine(name);
+
+            void AnimateText(string text)
+            {
+                int animationDelay = 50;
+
+                for (int i = 0; i <= 15; i++) // Fade-in
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = (ConsoleColor)i;
+                    Console.WriteLine(text);
+                    Thread.Sleep(animationDelay);
+                }
+
+                Thread.Sleep(1000); // Pause before fade-out
+
+                for (int i = 15; i >= 0; i--) // Fade-out
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = (ConsoleColor)i;
+                    Console.WriteLine(text);
+                    Thread.Sleep(animationDelay);
+                }
+            }
+            void DisplayTitle()
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine(name);
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine(motto);
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.ResetColor();
+            }
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(motto);
+
+            Console.WriteLine("=========================================================================================");
+            Console.ResetColor();
+
+
 
             while (true)
             {
@@ -40,7 +95,8 @@ namespace fitfinity
 
                         if (userManager.CreateUser(newUser, newPassword))
                         {
-
+                            Console.Clear() ;
+                            DisplayTitle();
                             Console.WriteLine("Choose an option:");
                             Console.WriteLine("1. Log In");
                             Console.WriteLine("2. Exit");
